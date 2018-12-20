7444047fc5fd0426004a267326ec124f7a711f4e
Mo <moh.alneama@yahoo.com>
2018-12-20 17:29:50 -0600

    logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,43 @@
 
-window.onload = function what(){
-var win = 0;
-document.getElementById("win").innerHTML = "win =  " +   win;
-var losse = 0;
-document.getElementById("losse").innerHTML ="losse = " + losse;
-var geuessleft = 9;
-document.getElementById("geuessleft").innerHTML ="geuessleft = " + geuessleft;
+window.onload = function what() {
+    var win = 0;
+    document.getElementById("win").innerHTML = "win =  " + win;
+    var losse = 0;
+    document.getElementById("losse").innerHTML = "losse = " + losse;
+    var geuessleft = 9;
+    document.getElementById("geuessleft").innerHTML = "geuessleft = " + geuessleft;
+    var wordlist = ["a"]
+    var compick = wordlist[Math.floor(Math.random() * wordlist.length)];
+    var correctword = [];
+    document.onkeypress = function (enterkey) {
+        var userGuess = enterkey.key;
+        document.getElementById("userGuess").innerHTML = "userGuess = " + userGuess;
 
-document.onkeypress = function(enterkey){
-    var userGuess = enterkey.key;
-document.getElementById("userGuess").innerHTML ="userGuess = " + userGuess;
-}
+       if (geuessleft !== 0) {
+            if (userGuess !== compick) {
+                geuessleft--;
+            }
+            else if (userGuess === compick) {
+                correctword.push(compick);
+                win++;
+                geuessleft = 9;
+            }
 
-var correctword = [];
-
-var wordlist = [ "cat", "dog", "tiger", "elephent"]
-var compick = wordlist[Math.floor(Math.random() * computerChoices.length)];
-
-for(i=0; i<wordlist.length; i++)
-{
-    for(i=0; i<compick; i++)
-    {
-        if(userGuess !== compick(i))
-        {
-            losse = loose + 1
         }
-        else if(userGuess == compick(i))
-        {
-            correctword(i)= userGuess;
-            document.getElementById("correctword").innerHTML ="correctword = " + correctword;
+        else if (geuessleft == 0) {
+            geuessleft = 9;
+            losse++;
+
         }
+
+        
+        document.getElementById("win").innerHTML = "win =  " + win;
+
+        document.getElementById("losse").innerHTML = "losse = " + losse;
+
+        document.getElementById("geuessleft").innerHTML = "geuessleft = " + geuessleft;
+        document.getElementById("correctword").innerHTML = "correctword = " + correctword;
+        
     }
-
-}
-
-
-
-
 }
 
